f6b9d43275d813dcad0226c06647835bca96e9b6
zwzhai <1325045440@qq.com>
2016-05-27 15:45:36 +0800

    step1

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var open = require('gulp-open');//open a url
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+
 
 var config = {
 	port:9000,
@@ -12,6 +14,10 @@ var config = {
 	paths:{
 		html:'./src/*.html',
 		js:'./src/**/*.js',
+		css:[
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theam.min.css'
+		],
 		dist:'./dist',
 		mainJs:'./src/main.js'
 	}
@@ -47,13 +53,18 @@ gulp.task('js',function(){
 	.pipe(gulp.dest(config.paths.dist+'/scripts'))
 	.pipe(connect.reload());
 
-})
+});
+gulp.task('css',function(){
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist+'/css'));
+});
 
 gulp.task('watch',function(){
 	gulp.watch(config.paths.html,['html']);
 	gulp.watch(config.paths.js,['js']);
 });
 
-gulp.task('default',['html','js','open','watch']);
+gulp.task('default',['html','js','css','open','watch']);
 
 
